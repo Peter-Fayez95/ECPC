86a8914f7de906f159f3bdb894fc777031202437
Peter <peterfayez1285@gmail.com>
2023-07-14 19:00:27 +0300

    Create FenwickTree.cc

---
+++ Data Structures/FenwickTree.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+// original array, tree array
+int n, q;
+ll arr[100005], tre[100005];
+
+//tree construction O(n)
+void construct(int k, ll x) {
+    while (k <= n) {
+        tre[k] += x;
+        k += k&-k;
+    }
+}
+
+// get cumulative sum (segment [1, k])
+// sum(a, b) = sum(b) - sum(a-1)
+ll sum(int k) {
+    ll s = 0;
+    while (k >= 1) {
+        s += tre[k];
+        k -= (k&(-k));
+    }
+    return s;
+}
+
+// query: add x to arr[k]
+// change x to your need for different functions
+// e.g. x = -arr[k] (query: arr[k] = 0)
+void add(int k, ll x) {
+    arr[k] += x;
+    while(k <= n) {
+        tre[k] += x;
+        k += k&-k;
+    }
+}
+
+int main() {
+    cin>>n>>q;
+    for (int i=1; i<=n; i++) {
+        cin>>arr[i];
+        tre[i]=0;
+    }
+    for (int i=1; i<=n; i++) {
+        construct(i, arr[i]);
+    }
+
+    // process queries O(log n)
+}
+
+
+
