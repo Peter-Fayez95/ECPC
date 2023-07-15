103911935a3acd5a9e83b6f19dad73079ddd8877
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-07-15 17:10:08 +0300

    Create UnionFind.cc

---
+++ Data Structures/UnionFind.cc
@@ -0,0 +1,20 @@
+//simplest form of DSU
+//rank herustic
+const int N = 2e5;
+int p[N];
+int r[N];
+
+void initPandR(){
+    for(int i=0;i<N;i++)p[i]=i,r[i]=1;
+}
+
+int findSet(int a){
+    return p[a] = ( p[a]==a?  a : findSet(p[a]) );
+}
+
+void unionSet(int a,int b){
+    a=findSet(a),b=findSet(b);
+    if(a==b) return;
+    if(r[a]==r[b])r[a]++;
+    r[a]>r[b] ? p[b]=a :p[a]=b;
+}
