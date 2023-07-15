3aa3560697a4cd02dc6afd9778d20cc228a758d8
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-07-15 17:44:11 +0300

    Create Debuging.cc

---
+++ CodeSnippets/Debuging.cc
@@ -0,0 +1,11 @@
+// debug to error stream
+//Warning!
+//Printing too much content to the error stream can cause TLE when submitting to an OJ.
+#define dbg(v)                                                                 \
+	cerr << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+
+
+//Checking for Out of Bounds
+// use vector::at() instead of vector::operator[]
+
+
