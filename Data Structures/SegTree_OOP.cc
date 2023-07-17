d5dba085ea790b4196c0b343a70d849594a37da0
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-07-17 18:25:21 +0300

    Update SegTree_OOP.cc

--- Data Structures/SegTree_OOP.cc
+++ Data Structures/SegTree_OOP.cc
@@ -4,43 +4,75 @@ using namespace std;
 
 #define ll long long
 
-struct segtree{
+struct item{
+    ll suffix, prefix, maxSum, totSum;
+};
+
+struct segTree{
     int size;
-    vector<ll> sums;
+    vector<item> vals;
+// ######### edit from here #########
+    item NEUTRAL_ELEMENT = {0,0,0,0};
+    item merge(item l, item r){
+      // merge left and right segments
+        return{
+          max({r.suffix, r.totSum + l.suffix}),
+          max({l.prefix, l.totSum + r.prefix}),
+          max({l.maxSum, r.maxSum, l.suffix + r.prefix }),
+          l.totSum + r.totSum
+        };
+    }
+    item single(int v){
+      //set value of a segment of length one
+        if(v){
+            return {v,v,v,v};
+        }
+        else{
+            return {0, 0, 0, v};
+        }
+    }
+// ########## to here ############
     void init(int n){
-        size=1;
+        size = 1;
         while(size < n) size *= 2;
-        sums.assign(2 * size, 0LL);
+        vals.resize(2 * size);
     }
-    void set(int i,ll v,int x,int lx,int rx){
-            if ( rx - lx == 1){
-                sums[x]=v;
-                return;
-            }
-            int m = (lx + rx) / 2;
-            if(i < m){
-                set(i, v, 2 * x + 1, lx, m);
-            }else{
-                set(i, v, 2 * x + 2, m, rx);
-            }
-            sums[x]=sums[2*x+1]+sums[2*x+2];
+    void set(int i, int v, int x, int lx, int rx){
+        if(rx - lx == 1) {
+            vals[x] = single(v);
+            return;
+        }
+        int m = (lx + rx) / 2;
+        if( i < m){
+            set( i, v, 2 * x + 1, lx, m);
+        }
+        else{
+            set( i, v, 2 * x + 2, m, rx);
+        }
+       vals[x] = merge(vals[2 * x +1  ], vals[2 * x + 2]);
     }
-    void set(int i,ll v){
+    void set(int i, int v){
         set(i, v, 0, 0, size);
     }
- 
-    ll sum(int l,int r,int x,int lx,int rx){
-        if(lx >= r || l >= rx) return 0;
-        if(lx >= l && rx <= r) return sums[x];
-        int m =(lx + rx) / 2;
-        ll s1 = sum (l, r, 2 * x + 1, lx, m);
-        ll s2 = sum (l, r, 2 * x + 2, m, rx);
-        return s1 + s2;
+
+    item getMax(int l, int r, int x, int lx, int rx){
+
+        if( lx >= r  || l >= rx  ) return NEUTRAL_ELEMENT;
+
+        if( lx >= l && rx <= r ) return vals[x];
+
+        int m = (lx + rx ) / 2;
+        auto lp = getMax(l, r, 2 * x + 1, lx, m);
+        auto rp = getMax(l, r, 2 * x + 2, m, rx);
+
+        return merge(lp, rp);
     }
-    ll sum(int l, int r){
-         return sum(l, r, 0, 0, size);
+    item getMax(int l, int r){
+        return getMax(l, r, 0, 0, size);
     }
+
 };
+
 int main(){
   int n = 1e5;
   segtree segtr;
