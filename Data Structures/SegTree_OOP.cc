952138df9a75605235c0056b8f657b28b1f1dc30
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-07-17 18:34:12 +0300

    Update SegTree_OOP.cc

--- Data Structures/SegTree_OOP.cc
+++ Data Structures/SegTree_OOP.cc
@@ -53,20 +53,20 @@ struct segTree{
         set(i, v, 0, 0, size);
     }
 
-    item getMax(int l, int r, int x, int lx, int rx){
+    item calc(int l, int r, int x, int lx, int rx){
 
         if( lx >= r  || l >= rx  ) return NEUTRAL_ELEMENT;
 
         if( lx >= l && rx <= r ) return vals[x];
 
         int m = (lx + rx ) / 2;
-        auto lp = getMax(l, r, 2 * x + 1, lx, m);
-        auto rp = getMax(l, r, 2 * x + 2, m, rx);
+        auto lp = calc(l, r, 2 * x + 1, lx, m);
+        auto rp = calc(l, r, 2 * x + 2, m, rx);
 
         return merge(lp, rp);
     }
-    item getMax(int l, int r){
-        return getMax(l, r, 0, 0, size);
+    item calc(int l, int r){
+        return calc(l, r, 0, 0, size);
     }
 
 };
