15bda9352c0a1e3792d33519ae6cffce9a16a94b
Peter <peterfayez1285@gmail.com>
2023-07-22 23:11:08 +0300

    Create builtin_functions.cc

---
+++ CodeSnippets/builtin_functions.cc
@@ -0,0 +1,15 @@
+// __builtin_clz(x): the number of zeros at the beginning of the number
+// __builtin_ctz(x): the number of zeros at the end of the number
+// __builtin_popcount(x): the number of ones in the number
+// __builtin_parity(x): the parity (even or odd) of the number of ones
+// __builtin_ffs(x): (the index of the least significant bits of x) + 1
+// __lg(x): the index of the highest set bit
+
+int x = 5328; // 00000000000000000001010011010000
+cout << __builtin_clz(x); // 19
+cout << __builtin_ctz(x); // 4
+cout << __builtin_popcount(x); // 5
+cout << __builtin_parity(x); // 1
+cout << __builtin_ffs(x); // 5
+cout << __lg(x); // 12 
+
