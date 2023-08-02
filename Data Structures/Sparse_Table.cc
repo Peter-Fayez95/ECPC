239b823e0751e76e7c2605b8d435b6b3345de2f0
Peter <peterfayez1285@gmail.com>
2023-08-02 23:57:16 +0300

    Update Sparse_Table.cc

--- Data Structures/Sparse_Table.cc
+++ Data Structures/Sparse_Table.cc
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 
+#include <bit>
+int log2_floor(unsigned long i) {
+    return std::bit_width(i) - 1;
+}
+
+// pre C++20
+int log2_floor(unsigned long long i) {
+    return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
+}
+
 // N is array size, K >= ceil(log N)
 const int K=25, N=200005;
 
