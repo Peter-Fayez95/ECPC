83495f00178aa60ad18ecf6ce95fc92ec9f21f05
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-08-02 12:27:06 +0300

    Create steps.cpp

---
+++ GetTemplateReady/steps.cpp
@@ -0,0 +1,13 @@
+/*
+* in the current directory, create a new project inside a new folder called template using clion if a project does not already exist, and open it
+* in your main.cpp write you template code
+* in clion terminal write
+
+1- to create
+*  for letter in {a..z}; do echo $letter; mkdir $letter; cp -R \template  $letter; done;
+
+2- now you have folders a to z with the template code
+
+3- every time you want to create a new code for a new submission for a problem, duplicte the main.cpp in that folder, rename to a number and work on the main.cpp
+4- do not add any <number>.cpp to cmakelist
+*/
