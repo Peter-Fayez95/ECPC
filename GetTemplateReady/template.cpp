381c454b99310396e3cc0da14f92c47be3bfb4a2
Peter <peterfayez1285@gmail.com>
2023-08-02 23:43:38 +0300

    Update template.cpp

--- GetTemplateReady/template.cpp
+++ GetTemplateReady/template.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-
 using namespace std;
 
-using namespace __gnu_pbds;
-
-template <class T>
-using ordered_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// find_by_order((int)k) returns iterator of the kth element
-// order_of_key((T)key) returns the number of elements less than this key
 
 #define ll long long
 #define all(x) (x).begin(), (x).end()
