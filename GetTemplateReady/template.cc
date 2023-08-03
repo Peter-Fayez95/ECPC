fa7eed5fb84fb8fe7b8f883c4157adc408eff821
AAST-MVPs <56249381+AAST-MVPs@users.noreply.github.com>
2023-08-04 01:08:57 +0300

    Update template.cc

--- GetTemplateReady/template.cc
+++ GetTemplateReady/template.cc
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define all(x) x.begin(), x.end()
 #define endl '\n'
 
-#define file                                                                   \
-  freopen("input.txt", "r", stdin);                                            \
-  freopen("output.txt", "w", stdout)
-
-#define OJudge(in, out)                                                        \
+#define file(in, out)                                                        \
   freopen(in, "r", stdin);                                                     \
   freopen(out, "w", stdout)
 
@@ -20,27 +15,26 @@ using namespace std;
   ios_base::sync_with_stdio(false)
 
 int tc;
+const bool MULTI_TESTS = false;
+const string IN = "input.txt";
+const string OUT = "output.txt";
 const int N = 1e4 + 10;
-const ll mod = 1e9 + 7;
+const ll MOD = 1e9 + 7;
 
 void pre();
-
-void solve() {}
-
+void solve(){
+    //your code goes here
+}
 int main() {
-  FIn;
-#ifndef ONLINE_JUDGE
-  file;
-#endif
-  //    OJudge(".in",".out");
-
-  tc = 1;
-  //    cin >> tc;
-  pre();
-  for (int i = 1; i <= tc; i++) {
-    // cout << "Case " << i <<": \n";
-    solve();
-  }
+    FIn;
+    file(IN.c_str(),OUT.c_str());
+    tc = 1;
+    if(MULTI_TESTS) cin >> tc;
+    pre();
+    for (int i = 1; i <= tc; i++) {
+        // cout << "Case " << i <<": \n";
+        solve();
+    }
 }
 
-void pre() {}
+void pre(){}
