48391d004e86416f99334d13ee0397080822f731
Peter <peterfayez1285@gmail.com>
2024-07-06 21:45:03 +0300

    Update polygons.cc

--- Geometry/polygons.cc
+++ Geometry/polygons.cc
@@ -6,10 +6,10 @@ pt translation;
 void polygon_area() {
     T tot = 0;
     n = points.size();
-    for (int i=0; i<n; i++) {
-        tot+= cross(points[i], points[(i+1)%n]);
+    for (int i = 0; i < n; i++) {
+        tot += cross(points[i], points[(i + 1) % n]);
     }
-    cout <<  abs(tot)/2.0 << "\n";
+    cout <<  abs(tot) / 2.0 << "\n";
 }
 
 // Pick's theorem
@@ -17,39 +17,39 @@ int pick_area() {
     // a:  number of integer points inside the polygon
     // b:  number of integer points on the polygon boundary
     int a = 0, b = 0;
-    count_integer_points(a,b);
-    return a + b/2 - 1;
+    count_integer_points(a, b);
+    return a + b / 2 - 1;
 }
 
 void count_integer_points(int &a, int &b) {
     T area = polygon_area();
     for (int i = 0; i < n; i++) {
-        pt z = points[(i+1)%n] - points[i];
+        pt z = points[(i + 1) % n] - points[i];
         int gc = gcd(z.x, z.y);
         b += abs(g);
     }
-    a = abs(area) + 1 - b/2;
+    a = abs(area) + 1 - b / 2;
 }
 
 bool isConvex(vector<pt> points) {
     /*check if polygon is convex*/
     bool hasPos = 0, hasNeg = 0;
     int n = points.size();
-    for (int i=0; i<n; i++) {
-        int o = orient(points[i], points[(i+1)%n], points[(i+2)%n]);
-        if (o > 0) hasPos=1;
-        else if (o < 0) hasNeg=1;
+    for (int i = 0; i < n; i++) {
+        int o = orient(points[i], points[(i + 1) % n], points[(i + 2) % n]);
+        if (o > 0) hasPos = 1;
+        else if (o < 0) hasNeg = 1;
     }
     return !(hasPos && hasNeg);
 }
 
 
-int sgn(ll x) { 
-    return x > 0 ? 1 : (x == 0 ? 0 : -1); 
+int sgn(T c) {
+    return c > 0 ? 1 : (c == 0 ? 0 : -1);
 }
 
 void prepare(vector<pt> &points) {
-    n = points.size();
+    int nn = points.size();
 
     // get minimum point
     int pos = min_element(points.begin(), points.end(), cmpX());
@@ -57,12 +57,12 @@ void prepare(vector<pt> &points) {
     // make p0 the minimum point
     rotate(points.begin(), points.begin() + pos, points.end());
 
-    n--;
+    nn--;
 
     // compute all vectors {p0, pi} after translating p0 to (0,0)
     // save them in seq
-    seq.resize(n);
-    for (int i = 0; i < n; i++)
+    seq.resize(nn);
+    for (int i = 0; i < nn; i++)
         seq[i] = points[i + 1] - points[0];
 
     // translation point = p0
@@ -71,8 +71,8 @@ void prepare(vector<pt> &points) {
 
 // check if point in triangle abc
 bool pointInTriangle(pt a, pt b, pt c, pt p) {
-    ll s1 = abs(cross(b-a, c-a));
-    ll s2 = abs(cross(a-p, b-p)) + abs(cross(b-p, c-p)) + abs(cross(c-p, a-p));
+    ll s1 = abs(cross(b - a, c - a));
+    ll s2 = abs(cross(a - p, b - p)) + abs(cross(b - p, c - p)) + abs(cross(c - p, a - p));
     return s1 == s2;
 }
 
@@ -86,13 +86,14 @@ bool check_boundary(int ind, pt p) {
     return onSegment(a, b, p);
 }
 
-// check if point is in polygon O(log n)
-// call prepare() before doing any queries
+// Checks if point is in polygon O(log n)
+// Call prepare() before doing any queries
+// IMPORTANT: Works only for convex polygons
 bool point_in_polygon(pt point) {
     // translate according to p0
     point = point - translation;
     int n = seq.size();
-    if (cross(seq[0], point) != 1 &&
+    if (cross(seq[0], point) != 0 &&
             sgn(cross(seq[0], point)) != sgn(cross(seq[0], seq[n - 1])))
         return false;
     if (cross(seq[n-1], point) != 0 &&
