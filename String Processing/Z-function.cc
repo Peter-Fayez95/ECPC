1efdda3ff05aa6a8843b292e488c26f573ad1f7c
Peter <peterfayez1285@gmail.com>
2024-07-11 03:10:11 +0300

    Create Z-function.cc

---
+++ String Processing/Z-function.cc
@@ -0,0 +1,20 @@
+// Calculates Z-function for string
+// z[i] is equal to the greatest number of characters starting from the position  
+// i that coincide with the first characters of the string
+vector<int> z_function(string s) {
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
