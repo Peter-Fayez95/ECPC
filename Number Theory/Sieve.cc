c004d8a764bc1af24760ecd0bd1153bd66a8c348
Peter <peterfayez1285@gmail.com>
2024-07-16 22:49:09 +0300

    Update Sieve.cc

--- Number Theory/Sieve.cc
+++ Number Theory/Sieve.cc
@@ -7,7 +7,7 @@ void sieve() {
     for (int i = 2; i < N; i += 2) {
         SPF[i] = 2;
     }
-    for (int i = 3; i < N; i += 2) {
+    for (int i = 3; i * i < N; i += 2) {
         if (SPF[i] == i) {
             for (int j = i * i; j < N; j += 2 * i)
                 SPF[j] = i;
