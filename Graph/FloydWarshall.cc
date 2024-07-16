e1f6a4584b1826235ba4e9d49ec70eb3cfb65176
Peter <peterfayez1285@gmail.com>
2024-07-16 21:37:40 +0300

    Update FloydWarshall.cc

--- Graph/FloydWarshall.cc
+++ Graph/FloydWarshall.cc
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
 
 # define INF 10000000
 int n, dist[105][105];
@@ -9,11 +7,12 @@ map<int,int> adj[105];
 //shortest path for all pairs
 //O(n^3)
 void floyd_warshall() {
-    for (int a=1; a<=n; a++) {
-        for (int b=1; b<=n; b++) {
-            if (a==b) dist[a][b]=0;
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            if (a == b) 
+                dist[a][b] = 0;
             else if (adj[a].count(b)) {
-                dist[a][b]=adj[a][b];
+                dist[a][b] = adj[a][b];
             }
             else {
                 dist[a][b] = INF;
@@ -21,10 +20,10 @@ void floyd_warshall() {
         }
     }
 
-    for (int k = 1; k<=n; k++) {
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=n; j++) {
-                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
