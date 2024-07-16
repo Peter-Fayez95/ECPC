eb53e40eadbdc17136bc4b56df3e9a1f30abf423
Peter <peterfayez1285@gmail.com>
2024-07-16 21:35:52 +0300

    Update BellmanFord.cc

--- Graph/BellmanFord.cc
+++ Graph/BellmanFord.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 # define INF 10000000
-int n, dist[200001];
+int n, dist[N];
 map<pair<int,int>, int> edges;
 
 //shortest path from starting node to all nodes
@@ -10,19 +10,20 @@ map<pair<int,int>, int> edges;
 //recieves edge list, O(nm)
 bool bellman_ford(int s) {
     // set distances to INF
-    for (int i=1; i<=n; i++) dist[i]=INF;
+    for (int i = 1; i <= n; i++) 
+        dist[i] = INF;
 
-    dist[s]=0;
-    for (int i=1; i<=n-1; i++) {
-        for (auto e:edges) {
+    dist[s] = 0;
+    for (int i = 1; i <= n - 1; i++) {
+        for (auto e : edges) {
             int a = e.first.first, b = e.first.second, w = e.second;
-            dist[b] = min(dist[b], dist[a]+w);
+            dist[b] = min(dist[b], dist[a] + w);
         }
     }
 
     //last loop to check for negative cycles
     //true if there is a negative cycle
-    for (auto e:edges) {
+    for (auto e : edges) {
         int a = e.first.first, b = e.first.second, w = e.second;
         if (dist[a] + w < dist[b]) {
             return 1;
