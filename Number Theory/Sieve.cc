1ee2089008543ff7018ef2e38519304e942e3dcc
Peter <peterfayez1285@gmail.com>
2024-07-16 21:09:10 +0300

    Update Sieve.cc

--- Number Theory/Sieve.cc
+++ Number Theory/Sieve.cc
@@ -1,20 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
-int SPF[1000005];
-
+const int N = 1e6 + 5;
 // O(n log log n)
 void sieve() {
-    for (int i=3; i<=1000000; i+=2) {
-        SPF[i]=i;
+    for (int i = 3; i < N; i += 2) {
+        SPF[i] = i;
     }
-    for (int i=2; i<=100000; i+=2) {
-        SPF[i]=2;
+    for (int i = 2; i < N; i += 2) {
+        SPF[i] = 2;
     }
-    for (int i = 3; i * i <= 1000000; i+=2) {
-        if (SPF[i]==i) {
-            for (int j = i * i; j <= 1000000; j += 2*i)
+    for (int i = 3; i < N; i += 2) {
+        if (SPF[i] == i) {
+            for (int j = i * i; j < N; j += 2 * i)
                 SPF[j] = i;
         }
     }
