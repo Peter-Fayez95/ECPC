6b9bff4fdb37e6b86b6b4398c307484b1a582faf
Peter <peterfayez1285@gmail.com>
2024-07-16 21:39:59 +0300

    Update TopologicalSort.cc

--- Graph/TopologicalSort.cc
+++ Graph/TopologicalSort.cc
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool vis[200005];
-vector<int> rslt;
-vector<int> adj[200005];
+bool vis[N];
+vector<int> rslt, adj[N];
 stack<int> st;
-int n,m,a,b;
+int n, m, a, b;
 
 void top_sort(int s) {
-    if (vis[s]) return;
-    vis[s]=1;
+    if (vis[s]) 
+        return;
+    vis[s] = 1;
     st.push(s);
-    for (int ne:adj[s]) {
+    for (int ne : adj[s]) {
         top_sort(ne);
     }
     rslt.push_back(s);
@@ -19,29 +19,30 @@ void top_sort(int s) {
 }
 
 int main() {
-    cin>>n>>m;
-    for (int i=0; i<m; i++) {
-        cin>>a>>b;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
         adj[a].push_back(b);
     }
-    for (int i=1; i<=n; i++) {
+    for (int i = 1; i <= n; i++) {
         if (!vis[i])
             top_sort(i);
     }
-    vector<int> ind(n+1);
+    vector<int> ind(n + 1);
     reverse(rslt.begin(), rslt.end());
-    for (int i=0; i<n; i++) {
-        ind[rslt[i]]=i+1;
+    for (int i = 0; i < n; i++) {
+        ind[rslt[i]] = i + 1;
     }
 
     // check if possible
-    for (int i=1; i<=n; i++) {
-        for (int ne:adj[i]) {
+    for (int i = 1; i <= n; i++) {
+        for (int ne : adj[i]) {
             if (ind[ne] <= ind[i]) {
-                cout<<"IMPOSSIBLE";
+                cout << "IMPOSSIBLE";
                 return;
             }
         }
     }
-    for (int ele:rslt) cout<<ele<<" ";
+    for (int ele : rslt) 
+        cout << ele << " ";
 }
