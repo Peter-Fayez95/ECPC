245dbf373294f18b75eba3a0a6486ff28d8e9279
Ahmed-waled <103792966+Ahmed-waled@users.noreply.github.com>
2024-08-15 16:36:26 +0300

    Create Articulation_Points.cc

---
+++ Graph/Articulation_Points.cc
@@ -0,0 +1,38 @@
+int n, m, u, v;
+int timer = 1; // don't forget to initilize timer each iteration
+int vis[N], tin[N], leastT[N];
+vector<int> g[N];
+
+void dfs_bridges(int node, int p){
+    vis[node] = true;
+    tin[node] = leastT[node] = timer++;
+    for(auto nei : g[node]){
+        if(nei == p)continue;
+        if (!vis[nei]) {
+            dfs_bridges(nei, node);
+        }
+        leastT[node] = min(leastT[node], leastT[nei]);
+    }
+    if(p != -1 && leastT[node] == tin[node]) {
+        // edge (node, p) is bridge
+        someFunctionWithBridge(node, p);
+    }
+
+}
+
+
+void solve() {
+    cin >> n >> m;
+    timer = 1;
+    for(int i = 1; i <= n; ++i)
+        g[i].clear(), vis[i] = false;
+
+    for(int i = 0; i < m; ++i){
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    dfs_bridges(1, -1);
+
+}
